62419add273b24e867ccba194f9dda6d765cb227
Nwokpor Gideon <gideonjohnson70@gmail.com>
2022-05-24 22:11:14 +0100

    Update to video and testimonials section

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,13 @@
 const hamburgerBtn = document.querySelector(".hero__header_hamburger");
 const overlay = document.querySelector(".overlay");
 const mobileMenu = document.querySelector(".hero__nav");
+const videoBtn = document.querySelector(".video__btn");
+const videoClip = document.querySelector(".video__clip");
+const closeVideo = document.querySelector(".video__clip_text");
 
 let menuOpen = false;
 
+// Functionality for hamburger button on mobile
 hamburgerBtn.addEventListener("click", function () {
   if (!menuOpen) {
     hamburgerBtn.classList.add("open");
@@ -31,4 +35,24 @@ overlay.addEventListener("click", function () {
   mobileMenu.classList.remove("fade-in");
   mobileMenu.classList.add("fade-out");
   menuOpen = false;
+});
+
+// Functionality for video button
+videoBtn.addEventListener("click", () => {
+  videoBtn.classList.add("active");
+  videoClip.classList.add("active");
+});
+
+closeVideo.addEventListener("click", () => {
+  videoBtn.classList.remove("active");
+  videoClip.classList.remove("active");
+});
+
+// Adding scrollable carousel feature in works section
+$(".works__main-carousel").flickity({
+  // options
+  cellAlign: "left",
+  wrapAround: true,
+  freeScroll: true, //makes carousel scrollable
+  autoPlay: true, //makes carousel scroll automatically
 });
